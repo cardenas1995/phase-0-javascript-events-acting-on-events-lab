565812e298d724fb0379f50a78ff622e16a7eb2e
Kevin <k.cardenas1795@gmail.com>
2024-01-21 12:10:44 -0500

    finish

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Your code here
+function moveDodgerLeft() {
+    const dodger = document.getElementById('dodger');
+    const currentPosition = parseInt(dodger.style.left);
+  
+    if (currentPosition > 0) {
+      const newPosition = currentPosition - 5; // Adjust the value as needed
+      dodger.style.left = `${newPosition}px`;
+    }
+  }
+  
+
+  function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    const currentPosition = parseInt(dodger.style.left);
+  
+    if (currentPosition < 360) {
+      const newPosition = currentPosition + 5; // Adjust the value as needed
+      dodger.style.left = `${newPosition}px`;
+    }
+  }
